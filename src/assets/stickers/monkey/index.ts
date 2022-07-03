c2e05d1b6614f673d32bab5b2e232795da6ba76e
Arrastia <arrastia@pm.me>
2022-07-03 19:57:52 +0200

    ADD - monkey close, tracking & peek json assets

--- src/assets/stickers/monkey/index.ts
+++ src/assets/stickers/monkey/index.ts
@@ -1,3 +1,6 @@
+import close from './monkey-close.json';
 import idle from './monkey-idle.json';
+import peek from './monkey-peek.json';
+import tracking from './monkey-tracking.json';
 
-export { idle };
+export { close, idle, peek, tracking };
