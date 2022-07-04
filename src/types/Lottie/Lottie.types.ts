6d8197d8c976acc211daf123f39d870e3ddb0fcc
Arrastia <arrastia@pm.me>
2022-07-04 20:37:00 +0200

    ADD - Lottie types

---
+++ src/types/Lottie/Lottie.types.ts
@@ -0,0 +1,30 @@
+import type { Player } from 'types/Player';
+
+export type LottieAPI = any;
+
+export type LottieEvents = any;
+
+export interface Lottie {
+  addEventListener: (reqId: any, eventName: any, callback: any) => void;
+  Api: LottieAPI;
+  clear: () => boolean;
+  clearPlayers: () => boolean;
+  destroy: (reqId: any) => void;
+  destroyWorkers: () => void;
+  events: LottieEvents;
+  frames: Map<any, any>;
+  hasFirstFrame: (reqId: any) => boolean;
+  init: (el: any, options: any) => boolean;
+  initApi: () => void;
+  isPaused: (reqId: any) => boolean;
+  isSafari: boolean;
+  isSupported: boolean;
+  loadAnimation: (options: any, callback: any) => void;
+  pause: (reqId: any) => void;
+  play: (reqId: any) => void;
+  players: Player[];
+  playSegments: (reqId: any, segments: any, forceFlag: any) => void;
+  preload: (fileId: any, stringData: any) => void;
+  removeEventListener: (reqId: any, eventName: any) => void;
+  WORKERS_LIMIT: number;
+}
---
+++ src/types/Lottie/index.ts
@@ -0,0 +1,1 @@
+export type { Lottie, LottieAPI, LottieEvents } from './Lottie.types';
