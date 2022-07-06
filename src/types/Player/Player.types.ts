b04316591c780e3db18b579cc41244e23e1d58c8
Arrastia <arrastia@pm.me>
2022-07-06 09:42:50 +0200

    UPDATE - Player types

--- src/types/Player/Player.types.ts
+++ src/types/Player/Player.types.ts
@@ -4,9 +4,24 @@ export interface PlayerOption {
 
 export interface Player {
   clamped: any;
+  fileId: string;
+  forceRender: boolean;
+  fps: any;
+  frameCount: number;
+  frameInterval: number;
+  frameNo: any;
+  frameQueue: any;
+  frameThen: number;
+  from: any;
   height: number;
+  nextFrameNo: boolean;
   options: PlayerOption;
   paused: boolean;
+  queueLength: number;
+  reqId: any;
+  rWorker: any;
+  segmentId: any;
+  to: any;
   url: string;
   width: number;
 }
