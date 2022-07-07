45236a901c4385ec3cd1db7ed68f697bc2126a0c
Arrastia <arrastia@pm.me>
2022-07-07 20:44:22 +0200

    UPDATE - LottieProvider to use createLottie util

--- src/providers/LottieProvider/LottieProvider.tsx
+++ src/providers/LottieProvider/LottieProvider.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import { LottieContext } from '../../contexts/LottieContext';
-import { buildRLottieObject } from './util';
+// import { buildRLottieObject } from './util';
 
 import type { ReactNode } from 'react';
+import { createLottie } from '../../utils/createLottie';
 
 export const LottieProvider = ({ children }: { children: ReactNode }) => {
-  const lottie = buildRLottieObject();
+  const lottie = createLottie();
 
   return <LottieContext.Provider value={lottie}>{children}</LottieContext.Provider>;
 };
